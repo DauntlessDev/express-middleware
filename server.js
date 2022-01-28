86fd15e52db62b2b03e47c61fe99ec1c4b81e4c5
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-01-28 19:43:47 +0800

    added code for basic logger middleware

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 
+app.use(logger)
+
 app.get('/', (req, res) => {
+    console.log('Home Page')
     res.send('Home Page')
 })
 
 app.get('/users', (req, res) => {
+    console.log('Users Page')
     res.send('Users Page')
 })
+
+function logger(req, res, next) {
+    console.log('Log')
+    next()
+}
 
 app.listen(3000)
