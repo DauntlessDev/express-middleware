161aef00b4ee36214fee9a25dc7dddfa41687f63
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-01-30 07:12:27 +0800

    improved auth scrupt

--- server.js
+++ server.js
@@ -19,8 +19,11 @@ function logger(req, res, next) {
 }
 
 function auth(req, res, next) {
-    console.log('Auth')
-    next()
+    if (req.query.admin === 'true'){
+        next()
+    }else{
+        res.send('No auth')
+    }
 }
 
 app.listen(3000)
